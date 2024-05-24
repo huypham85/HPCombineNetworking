530148ffd7c5dbdcc893cb882a4271cc9ce1bca2
Huy Pham <huypham8501@gmail.com>
2024-05-24 14:34:40 +0700

    add test for FormURLEncodeBody

--- Tests/HPCombineNetworkingTests/BodyParametersType/FormURLEncodedBodyParametersTests.swift
+++ Tests/HPCombineNetworkingTests/BodyParametersType/FormURLEncodedBodyParametersTests.swift
@@ -4,34 +4,26 @@ import XCTest
 import HPCombineNetworking
 
 class FormURLEncodedBodyParametersTests: XCTestCase {
-    func testJSONSuccess() throws {
+    func testURLSuccess() throws {
         let object = ["key1": 1, "key2": 2, "key3": 3]
         let parameters = FormURLEncodedBodyParameters(formObject: object)
         
         // verify the content-type
         XCTAssertEqual(parameters.contentType, "application/x-www-form-urlencoded")
         
-//        // verify the object after built RequestEntity
-//        guard case .data(let data) = try parameters.buildEntity() else {
-//            XCTFail()
-//            return
-//        }
-//        let dictionary = try JSONSerialization.jsonObject(with: data, options: [])
-//        XCTAssertEqual((dictionary as? [String: Int])?["key1"], 1)
-//        XCTAssertEqual((dictionary as? [String: Int])?["key2"], 2)
-//        XCTAssertEqual((dictionary as? [String: Int])?["key3"], 3)
+        // verify the object after built RequestEntity
+        do {
+            guard case .data(let data) = try parameters.buildEntity() else {
+                XCTFail()
+                return
+            }
+            
+            let createdObject = try URLEncodedSerialization.object(from: data, encoding: .utf8)
+            XCTAssertEqual(createdObject["key1"], "1")
+            XCTAssertEqual(createdObject["key2"], "2")
+            XCTAssertEqual(createdObject["key3"], "3")
+        } catch {
+            XCTFail()
+        }
     }
-    
-//    func testJSONFailure() {
-//        let object = NSObject()
-//        let parameters = JSONBodyParameters(JSONObject: object)
-//        
-//        
-//        // verify the error thrown by building entity
-//        XCTAssertThrowsError(try parameters.buildEntity()) { error in
-//            let nsError = error as NSError
-//            XCTAssertEqual(nsError.domain, NSCocoaErrorDomain)
-//            XCTAssertEqual(nsError.code, 3840)
-//        }
-//    }
 }
