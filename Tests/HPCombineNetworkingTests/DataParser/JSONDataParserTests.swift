eedc8c90cca3cc0693aa0eddf6d0994d562646fa
Huy Pham <huypham8501@gmail.com>
2024-05-24 16:54:13 +0700

    add tests for JSONDataParser

---
+++ Tests/HPCombineNetworkingTests/DataParser/JSONDataParserTests.swift
@@ -0,0 +1,80 @@
+
+import Foundation
+@testable import HPCombineNetworking
+import XCTest
+
+struct User: Codable {
+    let id: Int
+    let name: String
+}
+
+class JSONDataParserTests: XCTestCase {
+    var jsonDataParser: JSONDataParser!
+
+    override func setUp() {
+        super.setUp()
+        jsonDataParser = JSONDataParser(decoder: JSONDecoder())
+    }
+    
+    override func tearDown() {
+        jsonDataParser = nil
+        super.tearDown()
+    }
+    
+    func testParseValidData() {
+        let json = """
+        {
+            "id": 1,
+            "name": "Huy Pham"
+        }
+        """.data(using: .utf8)!
+        
+        do {
+            let user: User = try jsonDataParser.parse(json)
+            XCTAssertEqual(user.id, 1)
+            XCTAssertEqual(user.name, "Huy Pham")
+        } catch {
+            XCTFail("Expected to parse valid data, but failed with error: \(error)")
+        }
+    }
+    
+    func testParseInvalidData() {
+        let json = """
+        {
+            "id": "String",
+            "name": "Huy Pham"
+        }
+        """.data(using: .utf8)!
+        
+        XCTAssertThrowsError(try jsonDataParser.parse(json) as User) { error in
+            XCTAssertTrue(error is DecodingError, "Expected DecodingError but got \(type(of: error))")
+        }
+    }
+
+    func testParseDataWithSnakeCase() {
+        
+        struct Name: Codable {
+            let firstName: String
+            let lastName: String
+        }
+        
+        let json = """
+        {
+            "first_name": "Pham",
+            "last_name": "Huy"
+        }
+        """.data(using: .utf8)!
+        
+        let decoder = JSONDecoder()
+        decoder.keyDecodingStrategy = .convertFromSnakeCase
+        jsonDataParser = JSONDataParser(decoder: decoder)
+        
+        do {
+            let name: Name = try jsonDataParser.parse(json)
+            XCTAssertEqual(name.firstName, "Pham")
+            XCTAssertEqual(name.lastName, "Huy")
+        } catch {
+            XCTFail("Expected to parse valid data, but failed with error: \(error)")
+        }
+    }
+}
