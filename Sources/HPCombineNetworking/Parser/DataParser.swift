fa3d8ee16e2deef950003a9518505aaf2eb4dbed
Huy Pham <huypham8501@gmail.com>
2024-06-01 18:38:12 +0700

    make DataParser public access

--- Sources/HPCombineNetworking/Parser/DataParser.swift
+++ Sources/HPCombineNetworking/Parser/DataParser.swift
@@ -1,19 +1,19 @@
 
 import Foundation
 
-protocol DataParser {
+public protocol DataParser {
     var decoder: JSONDecoder { get }
     func parse<T: Decodable>(_ data: Data) throws -> T
 }
 
 public class JSONDataParser: DataParser {
-    var decoder: JSONDecoder
+    public var decoder: JSONDecoder
     
     public init(decoder: JSONDecoder) {
         self.decoder = decoder
     }
     
-    func parse<T>(_ data: Data) throws -> T where T : Decodable {
+    public func parse<T>(_ data: Data) throws -> T where T : Decodable {
         return try decoder.decode(T.self, from: data)
     }
 }
